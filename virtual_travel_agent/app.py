446419e8988c9a2a2bf390d47a27b5553f9928c5
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2023-06-14 23:09:01 +0100

    User authentication updated

--- virtual_travel_agent/app.py
+++ virtual_travel_agent/app.py
@@ -8,3 +8,32 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    # Import additional modules
+from flask import request
+
+# Update the registration route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Insert the user data into the database
+        conn = sqlite3.connect('travel_agent.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+                  (name, email, password))
+        conn.commit()
+        conn.close()
+
+        return "Registration successful! You can now <a href='/'>login</a>."
+
+    return render_template('index.html')
+
+# Remove the existing route for the home page
+@app.route('/')
+def home():
+    return render_template('login.html')
+
