e0a7dcc861076351ac07d55fae2acee33e8d777e
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2023-06-14 23:23:12 +0100

    User login updated

--- virtual_travel_agent/app.py
+++ virtual_travel_agent/app.py
@@ -37,3 +37,46 @@ def register():
 def home():
     return render_template('login.html')
 
+
+# Update the login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        # Check if the user exists in the database
+        conn = sqlite3.connect('travel_agent.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM users WHERE email=? AND password=?",
+                  (email, password))
+        user = c.fetchone()
+        conn.close()
+
+        if user:
+            session['user'] = {
+                'id': user[0],
+                'name': user[1],
+                'email': user[2]
+            }
+            return redirect('/dashboard')
+
+        return "Invalid email or password. <a href='/'>Try again</a>."
+
+    return render_template('index.html')
+
+# Create a route for the dashboard page
+@app.route('/dashboard')
+def dashboard():
+    if 'user' in session:
+        return f"Welcome, {session['user']['name']}! This is your dashboard."
+    else:
+        return redirect('/')
+
+# Create a route for logout
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
+
+
